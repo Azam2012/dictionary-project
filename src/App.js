efed5a4774efefdd8d0224598d4de9a23ea92477
Azam2012 <ghasemighasemi195@gmail.com>
2021-10-20 01:14:33 +0200

    Created Dictionary.js and Dictionary.css and Search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import logo from "./logo.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            {" "}
+            This project was coded by Azam and is open-soursed on {""}
+            <a
+              href="https://github.com/Azam2012/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              GitHub{" "}
+            </a>
+            and{" "}
+            <a href="https://sleepy-kalam-c0ff15.netlify.app/" rel="noreferrer">
+              {" "}
+              hosted on Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
