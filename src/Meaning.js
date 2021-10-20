7fbb502c4adc16e591c06dfe43aa6a489b4b960d
Azam2012 <ghasemighasemi195@gmail.com>
2021-10-21 00:09:59 +0200

    Rendered the word meanings and example

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,5 +2,20 @@ import React from "react";
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  return "Hello from meaning";
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <br />
+            <em>
+              <p>{definition.example}</p>
+            </em>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
