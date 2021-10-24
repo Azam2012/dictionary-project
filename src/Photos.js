6851e871c8de773d7412bfb224b4fd2628e351cc
Azam2012 <ghasemighasemi195@gmail.com>
2021-10-24 23:39:26 +0200

    Debugged

--- src/Photos.js
+++ src/Photos.js
@@ -15,7 +15,11 @@ export default function Photos(props) {
                   alt="original"
                   rel="noreferrer"
                 >
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="landscape"
+                  />
                 </a>
               </div>
             );
