61c8e7827169022ec580ef3b7b03a91c5320200c
Azam2012 <ghasemighasemi195@gmail.com>
2021-10-25 16:39:42 +0200

    Changed the default word to Welcome

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <main>
-          <Dictionary defaultKeyword="hello" />
+          <Dictionary defaultKeyword="Welcome" />
         </main>
         <footer className="App-footer">
           <small>
